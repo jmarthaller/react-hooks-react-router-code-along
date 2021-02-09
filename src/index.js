7d5311f7c036d62a0f2206eec105c8e2eca04ad5
Jacob Marthaller <https://github.com/jmarthaller>
2021-02-09 14:40:35 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,133 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {  BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
+import Home from './Home';
 
-function Home() {
+const linkStyles = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none', 
+  color: 'white',
+};
+
+function NavBar() {
   return (
     <div>
-      <h1>Home!</h1>
+      <NavLink
+        to="/"
+        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+        exact
+        /* add styling to Navlink */
+        style={linkStyles}
+        /* add prop for activeStyle */
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Login
+      </NavLink>
+      <NavLink
+        to="/signup"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Signup
+      </NavLink>
+
+      <NavLink
+        to="/messages"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Messages
+      </NavLink>
+      
     </div>
   );
 }
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+
+function About() {
+  return <h2>This is my about component</h2>;
+}
+
+function Signup() {
+  return <h3>ooooo such signup</h3>
+}
+
+
+function Messages() {
+  return (
+    <ul>
+      <li>signup 1</li>
+      <li>signup 2</li>
+      <li>signup 3</li>
+    </ul>
+  )
+}
+
+function Login() {
+  return <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+}
+
+ReactDOM.render(
+  <Router>
+    <NavBar />
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/messages">
+        <Messages />
+      </Route>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
